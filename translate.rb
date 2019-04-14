88764017c2f87bde2296e5f75b754a1b7d57bab5
joe <kinjoyuji@gmail.com>
2019-04-14 18:46:25 +1000

    localで任意の日本語queryをenに変換する実装

--- translate.rb
+++ translate.rb
@@ -4,20 +4,20 @@ Bundler.require
 require 'dotenv'
 Dotenv.load
 
-# require "google/cloud/translate"
+require "google/cloud/translate"
 require 'sinatra'
 
-# Google::Cloud::Translate.configure do |config|
-#   config.project_id  = ENV['PROJECT_ID']
-#   config.credentials = "./credentials.json"
-# end
-# translate = Google::Cloud::Translate.new
-# translation = translate.translate "こんにちは。私は今、カフェにいます", to: "en"
+Google::Cloud::Translate.configure do |config|
+  config.project_id  = ENV['PROJECT_ID']
+  config.credentials = "./credentials.json"
+end
+translate = Google::Cloud::Translate.new
 
 set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 80
 
 get '/' do
-  target = ENV['TARGET'] || 'World'
-  "Hello #{target}!\n"
+  query = params['query'].to_s || ''
+  translation = translate.translate query, to: "en"
+  translation.text
 end
