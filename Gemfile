63ae80e5ac57b63ad7c2d87a5a3d7daaeeb2ca56
joe <kinjoyuji@gmail.com>
2019-04-14 17:10:42 +1000

    add sinatra

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'google-cloud-translate'
 gem 'dotenv'
+gem 'sinatra'
+gem 'rack'
--- translate.rb
+++ translate.rb
@@ -5,6 +5,7 @@ require 'dotenv'
 Dotenv.load
 
 require "google/cloud/translate"
+require 'sinatra'
 
 Google::Cloud::Translate.configure do |config|
   config.project_id  = ENV['PROJECT_ID']
@@ -16,3 +17,10 @@ translate = Google::Cloud::Translate.new
 translation = translate.translate "こんにちは。私は今、カフェにいます", to: "en"
 
 pp translation.text
+
+set :bind, '0.0.0.0'
+
+get '/' do
+  target = ENV['TARGET'] || 'World'
+  "Hello #{target}!\n"
+end
